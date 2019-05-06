0d5eb360698b5a1a973c6107908f90bf686d3770
Andy Martinez <martinez1751@gmail.com>
2019-05-06 22:29:03 +0000

    home layout

--- src/js/component/about.js
+++ src/js/component/about.js
@@ -1,30 +1,163 @@
-//create your first component
 import React from "react";
 import avatar from "../../img/avatar.jpeg";
-import SkillsProgress from "./stackProgress";
 
-//include images into your bundle
-const About = () => {
-	return (
-		<React.Fragment>
-			<div className="col d-flex justify-content-between">
-				<img className="avatarImg" src={avatar} />
-				<img className="avatarImg" src={avatar} />
-				<img className="avatarImg" src={avatar} />
-				<img className="avatarImg" src={avatar} />
-				<img className="avatarImg" src={avatar} />
-			</div>
+import PropTypes from "prop-types";
 
-			<div className="w-100" />
-			<div className="col-6 m-auto">
-				<img className="avatarImg" src={avatar} />
-				<p>Web developer from Miami, FL </p>
+export default class About extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div id="about-container" className="content-containers container text-center mt-5">
+				<h1>About</h1>
+				<div className="row mt-5">
+					<div className="col-6 col-sm-6 col-md-3">
+						<img
+							className="img-fluid"
+							src={avatar}
+							style={{
+								borderRadius: 50 + "%",
+								height: 100 + "px",
+								width: 100 + "px"
+							}}
+						/>
+						<h4>React</h4>
+					</div>
+					<div className="col-6 col-sm-6 col-md-3">
+						<img
+							className="img-fluid"
+							src={avatar}
+							style={{
+								borderRadius: 50 + "%",
+								height: 100 + "px",
+								width: 100 + "px"
+							}}
+						/>
+						<h4>Vanilla Js</h4>
+					</div>
+					<div className="col-6 col-sm-6 col-md-3">
+						<img
+							className="img-fluid"
+							src={avatar}
+							style={{
+								borderRadius: 50 + "%",
+								height: 100 + "px",
+								width: 100 + "px"
+							}}
+						/>
+						<h4>PHP</h4>
+					</div>
+					<div className="col-6 col-sm-6 col-md-3">
+						<img
+							className="img-fluid"
+							src={avatar}
+							style={{
+								borderRadius: 50 + "%",
+								height: 100 + "px",
+								width: 100 + "px"
+							}}
+						/>
+						<h4>Wordpress</h4>
+					</div>
+				</div>
+
+				<div className="row" style={{ marginTop: 7 + "rem" }}>
+					<div className="col-12 col-lg-6 hidden">
+						<img
+							className="img-fluid"
+							src={avatar}
+							style={{
+								borderRadius: 50 + "%",
+								height: 100 + "px",
+								width: 100 + "px"
+							}}
+						/>
+						<p className="mytext pt-3">Web Developer from Miami, FL.</p>
+					</div>
+					<div className="col-12 col-sm-12 col-md-12 col-lg-6 ">
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-80-bar"
+								role="progressbar"
+								style={{ width: 80 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								HTML5
+							</div>
+						</div>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-80-bar"
+								role="progressbar"
+								style={{ width: 80 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								CSS3
+							</div>
+						</div>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-90-bar"
+								role="progressbar"
+								style={{ width: 90 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								Javascript
+							</div>
+						</div>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-60-bar"
+								role="progressbar"
+								style={{ width: 60 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								PHP
+							</div>
+						</div>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-80-bar"
+								role="progressbar"
+								style={{ width: 80 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								ReactJS
+							</div>
+						</div>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-80-bar"
+								role="progressbar"
+								style={{ width: 80 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								VanillaJS
+							</div>
+						</div>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-60-bar"
+								role="progressbar"
+								style={{ width: 60 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								JQuery
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
-			<div className="col-6 m-auto">
-				<SkillsProgress />
-			</div>
-		</React.Fragment>
-	);
+		);
+	}
+}
+
+About.propTypes = {
+	bounceLeft: PropTypes.string,
+	fadeInLeft: PropTypes.string,
+	fadeInRight: PropTypes.string,
+	fadeIn: PropTypes.string,
+	tada: PropTypes.string
 };
-
-export default About;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,8 +2,6 @@ import React from "react";
 import HeroView from "../component/heroView";
 import "../../styles/home.scss";
 
-import avatar from "../../img/avatar.jpeg";
-
 import About from "../component/about";
 
 export class Home extends React.Component {
@@ -14,52 +12,20 @@ export class Home extends React.Component {
 		return (
 			<React.Fragment>
 				<HeroView />
-				<div className="container-fluid">
-					<div className="row">
-						<About />
-					</div>
-					<div className="row">
-						<div className="col">
-							<h2>TITLE HEADING</h2>
-							<h5>Title description, Dec 7, 2017</h5>
-							<div className="fakeimg">Fake Image</div>
-							<p>Some text..</p>
-							<p>
-								Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing
-								elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-								minim veniam, quis nostrud exercitation ullamco.
-							</p>
-							<br />
-							<h2>TITLE HEADING</h2>
-							<h5>Title description, Dec 7, 2017</h5>
-							<div className="fakeimg">Fake Image</div>
-							<p>Some text..</p>
-							<p>
-								Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing
-								elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-								minim veniam, quis nostrud exercitation ullamco.
-							</p>
-							<br />
-							<h2>TITLE HEADING</h2>
-							<h5>Title description, Dec 7, 2017</h5>
-							<div className="fakeimg">Fake Image</div>
-							<p>Some text..</p>
-							<p>
-								Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing
-								elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-								minim veniam, quis nostrud exercitation ullamco.
-							</p>
-							<br />
-							<h2>TITLE HEADING</h2>
-							<h5>Title description, Sep 2, 2017</h5>
-							<div className="fakeimg">Fake Image</div>
-							<p>Some text..</p>
-							<p>
-								Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing
-								elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-								minim veniam, quis nostrud exercitation ullamco.
-							</p>
-						</div>
+
+				<About />
+
+				<div className="row">
+					<div className="col text-center">
+						<h2>Projects</h2>
+						<h5>Title description, Dec 7, 2017</h5>
+						<div className="fakeimg">Fake Image</div>
+						<p>Some text..</p>
+						<p>
+							Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit,
+							sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco.
+						</p>
 					</div>
 				</div>
 
