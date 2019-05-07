50bcd483c0302b5ae7889e99175a04f8d6979053
Andy Martinez <martinez1751@gmail.com>
2019-05-07 15:59:27 +0000

    about layout

--- src/js/component/about.js
+++ src/js/component/about.js
@@ -1,5 +1,6 @@
 import React from "react";
 import avatar from "../../img/avatar.jpeg";
+import { Progress } from "reactstrap";
 
 import PropTypes from "prop-types";
 
@@ -14,51 +15,23 @@ export default class About extends React.Component {
 				<h1>About</h1>
 				<div className="row mt-5">
 					<div className="col-6 col-sm-6 col-md-3">
-						<img
-							className="img-fluid"
-							src={avatar}
-							style={{
-								borderRadius: 50 + "%",
-								height: 100 + "px",
-								width: 100 + "px"
-							}}
-						/>
+						<i className="fab fa-react fa-5x" />
+
 						<h4>React</h4>
 					</div>
 					<div className="col-6 col-sm-6 col-md-3">
-						<img
-							className="img-fluid"
-							src={avatar}
-							style={{
-								borderRadius: 50 + "%",
-								height: 100 + "px",
-								width: 100 + "px"
-							}}
-						/>
+						<i className="fab fa-js-square fa-5x" />
+
 						<h4>Vanilla Js</h4>
 					</div>
 					<div className="col-6 col-sm-6 col-md-3">
-						<img
-							className="img-fluid"
-							src={avatar}
-							style={{
-								borderRadius: 50 + "%",
-								height: 100 + "px",
-								width: 100 + "px"
-							}}
-						/>
-						<h4>PHP</h4>
+						<i className="fab fa-html5 fa-5x" />
+
+						<h4>HTML</h4>
 					</div>
 					<div className="col-6 col-sm-6 col-md-3">
-						<img
-							className="img-fluid"
-							src={avatar}
-							style={{
-								borderRadius: 50 + "%",
-								height: 100 + "px",
-								width: 100 + "px"
-							}}
-						/>
+						<i className="fab fa-wordpress-simple fa-5x" />
+
 						<h4>Wordpress</h4>
 					</div>
 				</div>
@@ -70,83 +43,37 @@ export default class About extends React.Component {
 							src={avatar}
 							style={{
 								borderRadius: 50 + "%",
-								height: 100 + "px",
-								width: 100 + "px"
+								height: 220 + "px",
+								width: 220 + "px"
 							}}
 						/>
 						<p className="mytext pt-3">Web Developer from Miami, FL.</p>
 					</div>
 					<div className="col-12 col-sm-12 col-md-12 col-lg-6 ">
-						<div className="progress mb-3">
-							<div
-								className="progress-bar fill-80-bar"
-								role="progressbar"
-								style={{ width: 80 + "%" }}
-								aria-valuemin="0"
-								aria-valuemax="100">
-								HTML5
-							</div>
-						</div>
-						<div className="progress mb-3">
-							<div
-								className="progress-bar fill-80-bar"
-								role="progressbar"
-								style={{ width: 80 + "%" }}
-								aria-valuemin="0"
-								aria-valuemax="100">
-								CSS3
-							</div>
-						</div>
-						<div className="progress mb-3">
-							<div
-								className="progress-bar fill-90-bar"
-								role="progressbar"
-								style={{ width: 90 + "%" }}
-								aria-valuemin="0"
-								aria-valuemax="100">
-								Javascript
-							</div>
-						</div>
-						<div className="progress mb-3">
-							<div
-								className="progress-bar fill-60-bar"
-								role="progressbar"
-								style={{ width: 60 + "%" }}
-								aria-valuemin="0"
-								aria-valuemax="100">
-								PHP
-							</div>
-						</div>
-						<div className="progress mb-3">
-							<div
-								className="progress-bar fill-80-bar"
-								role="progressbar"
-								style={{ width: 80 + "%" }}
-								aria-valuemin="0"
-								aria-valuemax="100">
-								ReactJS
-							</div>
-						</div>
-						<div className="progress mb-3">
-							<div
-								className="progress-bar fill-80-bar"
-								role="progressbar"
-								style={{ width: 80 + "%" }}
-								aria-valuemin="0"
-								aria-valuemax="100">
-								VanillaJS
-							</div>
-						</div>
-						<div className="progress mb-3">
-							<div
-								className="progress-bar fill-60-bar"
-								role="progressbar"
-								style={{ width: 60 + "%" }}
-								aria-valuemin="0"
-								aria-valuemax="100">
-								JQuery
-							</div>
-						</div>
+						<Progress animated color="info" value={80}>
+							React
+						</Progress>
+						<br />
+						<Progress animated color="info" value={85}>
+							JavaScript
+						</Progress>
+						<br />
+						<Progress animated color="info" value={80}>
+							HTML
+						</Progress>
+						<br />
+						<Progress animated color="info" value={80}>
+							CSS
+						</Progress>
+						<br />
+						<Progress animated color="info" value={75}>
+							WordPress
+						</Progress>
+						<br />
+						<Progress animated color="info" value={70}>
+							PHP
+						</Progress>
+						<br />
 					</div>
 				</div>
 			</div>
@@ -154,10 +81,15 @@ export default class About extends React.Component {
 	}
 }
 
-About.propTypes = {
-	bounceLeft: PropTypes.string,
-	fadeInLeft: PropTypes.string,
-	fadeInRight: PropTypes.string,
-	fadeIn: PropTypes.string,
-	tada: PropTypes.string
+Progress.propTypes = {
+	multi: PropTypes.bool,
+	bar: PropTypes.bool, // used in combination with multi
+	tag: PropTypes.string,
+	value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+	max: PropTypes.oneOf([PropTypes.string, PropTypes.number]),
+	animated: PropTypes.bool,
+	striped: PropTypes.bool,
+	color: PropTypes.string,
+	className: PropTypes.string,
+	barClassName: PropTypes.string // used to add class to the inner progress-bar element
 };
