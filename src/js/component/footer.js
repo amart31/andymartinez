8e07a46cb5f31d4b89bb51a905e1a2e75245617e
Andy Martinez <martinez1751@gmail.com>
2019-05-08 23:54:59 +0000

    user

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
+			Made with <i className="fa fa-heart fa-3x text-danger" /> by{" "}
 			<a href="https://github.com/amart31">Andy Martinez</a>
 		</p>
 	</footer>
