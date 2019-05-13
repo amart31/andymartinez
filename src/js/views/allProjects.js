c3b1d28051b90b423c894632d5c7d92853ae7517
Andy Martinez <martinez1751@gmail.com>
2019-05-13 22:23:43 +0000

    container-fluid

--- src/js/views/allProjects.js
+++ src/js/views/allProjects.js
@@ -59,269 +59,273 @@ export class ProjectsList extends React.Component {
 						</div>
 					</div>
 				</div>
-				<div className="row m-3">
-					<div className="col-xs-12 col-sm-12 col-md-6 col-lg-4">
-						<div>
-							<Card className="projectCard">
-								<CardImg
-									top
-									width="200"
-									height="200"
-									className="card-img"
-									src="https://amart31.github.io/andymartinez/react-closet.png"
-									alt="Card image cap"
-								/>
-								<CardBody>
-									<CardTitle>React Closet app</CardTitle>
-									<CardSubtitle>React | WP | Vanilla Js | PHP | Bootstrap | HTML & CSS</CardSubtitle>
+				<div className="container-fluid">
+					<div className="row m-3">
+						<div className="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+							<div>
+								<Card className="projectCard">
+									<CardImg
+										top
+										width="200"
+										height="200"
+										className="card-img"
+										src="https://amart31.github.io/andymartinez/react-closet.png"
+										alt="Card image cap"
+									/>
+									<CardBody>
+										<CardTitle>React Closet app</CardTitle>
+										<CardSubtitle>
+											React | WP | Vanilla Js | PHP | Bootstrap | HTML & CSS
+										</CardSubtitle>
 
-									<Button color="info" onClick={this.toggleModalReactCloset}>
-										See More
-									</Button>
-								</CardBody>
+										<Button color="info" onClick={this.toggleModalReactCloset}>
+											See More
+										</Button>
+									</CardBody>
 
-								<Modal
-									isOpen={this.state.modalReactCloset}
-									toggle={this.toggleModalReactCloset}
-									style={{ color: "#4a82db" }}>
-									<ModalHeader toggle={this.toggleModalReactCloset}>React Closet App</ModalHeader>
-									<ModalBody>
-										<p> React Closet App </p>
-										<p> React </p>
-										<p> Vanilla Js </p>
-										<p> WordPress </p>
-										<p> PHP </p>
-									</ModalBody>
-									<ModalFooter>
-										<Link to="#">
-											<Button color="info">Live Demo</Button>
-										</Link>
-									</ModalFooter>
-								</Modal>
-							</Card>
+									<Modal
+										isOpen={this.state.modalReactCloset}
+										toggle={this.toggleModalReactCloset}
+										style={{ color: "#4a82db" }}>
+										<ModalHeader toggle={this.toggleModalReactCloset}>React Closet App</ModalHeader>
+										<ModalBody>
+											<p> React Closet App </p>
+											<p> React </p>
+											<p> Vanilla Js </p>
+											<p> WordPress </p>
+											<p> PHP </p>
+										</ModalBody>
+										<ModalFooter>
+											<Link to="#">
+												<Button color="info">Live Demo</Button>
+											</Link>
+										</ModalFooter>
+									</Modal>
+								</Card>
+							</div>
+						</div>
+
+						<div className=" col-xs-12 col-sm-12 col-md-6 col-lg-4">
+							<div>
+								<Card className="projectCard">
+									<CardImg
+										top
+										width="200"
+										height="200"
+										className="card-img"
+										src="https://amart31.github.io/andymartinez/instagram-bstrap.png"
+										alt="Card image cap"
+									/>
+									<CardBody>
+										<CardTitle>Instagram Photofeed</CardTitle>
+										<CardSubtitle>HTML | CSS | Bootstrap</CardSubtitle>
+
+										<Button color="info" onClick={this.toggleModalToDoApp}>
+											See More
+										</Button>
+									</CardBody>
+
+									<Modal
+										isOpen={this.state.modalToDoApp}
+										toggle={this.toggleModalToDoApp}
+										style={{ color: "#4a82db" }}>
+										<ModalHeader toggle={this.toggleModalToDoApp}>Instagram Feed Clone</ModalHeader>
+										<ModalBody>
+											<p> Instagram Feed Clone </p>
+											<p> React </p>
+											<p> Vanilla Js </p>
+											<p> Bootstrap </p>
+											<p> HTML & CSS </p>
+										</ModalBody>
+										<ModalFooter>
+											<a href="https://amart31.github.io/Instagram-photo-grid-clone/">
+												<Button color="info">Live Demo</Button>
+											</a>
+										</ModalFooter>
+									</Modal>
+								</Card>
+							</div>
+						</div>
+
+						<div className="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+							<div>
+								<Card className="projectCard">
+									<CardImg
+										top
+										width="200"
+										height="200"
+										className="card-img"
+										src="https://amart31.github.io/andymartinez/meetup-clone.png"
+										alt="Card image cap"
+									/>
+									<CardBody>
+										<CardTitle>meetup.com Clone</CardTitle>
+										<CardSubtitle>React | Vanilla Js | WP | PHP</CardSubtitle>
+
+										<Button color="info" onClick={this.toggleModalMeetupClone}>
+											See More
+										</Button>
+									</CardBody>
+
+									<Modal
+										isOpen={this.state.modalMeetupClone}
+										toggle={this.toggleModalMeetupClone}
+										style={{ color: "#4a82db" }}>
+										<ModalHeader toggle={this.toggleModalMeetupClone}>meetup.com Clone</ModalHeader>
+										<ModalBody>
+											<p> meetup.com Clone </p>
+											<p> React </p>
+											<p> Vanilla Js </p>
+											<p> Bootstrap </p>
+											<p> WordPress </p>
+											<p> PHP </p>
+										</ModalBody>
+										<ModalFooter>
+											<Link to="#">
+												<Button color="info">Live Demo</Button>
+											</Link>
+										</ModalFooter>
+									</Modal>
+								</Card>
+							</div>
 						</div>
 					</div>
+					<div className="row m-2">
+						<div className="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+							<div>
+								<Card className="projectCard">
+									<CardImg
+										top
+										width="200"
+										height="200"
+										className="card-img"
+										src="https://amart31.github.io/andymartinez/excuse-generator.png"
+										alt="Card image cap"
+									/>
+									<CardBody>
+										<CardTitle>Excuse Generator site</CardTitle>
+										<CardSubtitle>Vanilla Js | HTML | CSS | Bootstrap</CardSubtitle>
 
-					<div className=" col-xs-12 col-sm-12 col-md-6 col-lg-4">
-						<div>
-							<Card className="projectCard">
-								<CardImg
-									top
-									width="200"
-									height="200"
-									className="card-img"
-									src="https://amart31.github.io/andymartinez/instagram-bstrap.png"
-									alt="Card image cap"
-								/>
-								<CardBody>
-									<CardTitle>Instagram Photofeed</CardTitle>
-									<CardSubtitle>HTML | CSS | Bootstrap</CardSubtitle>
+										<Button color="info" onClick={this.toggleModalReactCloset}>
+											See More
+										</Button>
+									</CardBody>
 
-									<Button color="info" onClick={this.toggleModalToDoApp}>
-										See More
-									</Button>
-								</CardBody>
+									<Modal
+										isOpen={this.state.modalReactCloset}
+										toggle={this.toggleModalReactCloset}
+										style={{ color: "#4a82db" }}>
+										<ModalHeader toggle={this.toggleModalReactCloset}>
+											Excuse Generator Vanilla Js
+										</ModalHeader>
+										<ModalBody>
+											<p> Excuse Generator site </p>
+											<p> React </p>
+											<p> Vanilla Js </p>
+											<p> Bootstrap </p>
+											<p> HTML & CSS </p>
+										</ModalBody>
+										<ModalFooter>
+											<a href="https://amart31.github.io/excuse-generator/">
+												<Button color="info">Live Demo</Button>
+											</a>
+										</ModalFooter>
+									</Modal>
+								</Card>
+							</div>
+						</div>
 
-								<Modal
-									isOpen={this.state.modalToDoApp}
-									toggle={this.toggleModalToDoApp}
-									style={{ color: "#4a82db" }}>
-									<ModalHeader toggle={this.toggleModalToDoApp}>Instagram Feed Clone</ModalHeader>
-									<ModalBody>
-										<p> Instagram Feed Clone </p>
-										<p> React </p>
-										<p> Vanilla Js </p>
-										<p> Bootstrap </p>
-										<p> HTML & CSS </p>
-									</ModalBody>
-									<ModalFooter>
-										<a href="https://amart31.github.io/Instagram-photo-grid-clone/">
-											<Button color="info">Live Demo</Button>
-										</a>
-									</ModalFooter>
-								</Modal>
-							</Card>
+						<div className="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+							<div>
+								<Card className="projectCard">
+									<CardImg
+										top
+										width="200"
+										height="200"
+										className="card-img"
+										src="https://amart31.github.io/andymartinez/instagram-bstrap.png"
+										alt="Card image cap"
+									/>
+									<CardBody>
+										<CardTitle>Instagram Photofeed</CardTitle>
+										<CardSubtitle>HTML, CSS, & Bootstrap</CardSubtitle>
+
+										<Button color="info" onClick={this.toggleModalToDoApp}>
+											See More
+										</Button>
+									</CardBody>
+
+									<Modal
+										isOpen={this.state.modalToDoApp}
+										toggle={this.toggleModalToDoApp}
+										style={{ color: "#4a82db" }}>
+										<ModalHeader toggle={this.toggleModalToDoApp}>React Closet App</ModalHeader>
+										<ModalBody>
+											<p> React Modal </p>
+											<p> React </p>
+											<p> Vanilla Js </p>
+											<p> Bootstrap </p>
+											<p> HTML & CSS </p>
+											<p> WordPress </p>
+											<p> PHP </p>
+										</ModalBody>
+										<ModalFooter>
+											<a href="https://amart31.github.io/Instagram-photo-grid-clone/">
+												<Button color="info">Live Demo</Button>
+											</a>
+										</ModalFooter>
+									</Modal>
+								</Card>
+							</div>
+						</div>
+
+						<div className="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+							<div>
+								<Card className="projectCard">
+									<CardImg
+										top
+										width="200"
+										height="200"
+										className="card-img"
+										src="https://amart31.github.io/andymartinez/meetup-clone.png"
+										alt="Card image cap"
+									/>
+									<CardBody>
+										<CardTitle>meetup.com Clone</CardTitle>
+										<CardSubtitle>React, Vanilla Js, WP, PHP</CardSubtitle>
+
+										<Button color="info" onClick={this.toggleModalMeetupClone}>
+											See More
+										</Button>
+									</CardBody>
+
+									<Modal
+										isOpen={this.state.modalMeetupClone}
+										toggle={this.toggleModalMeetupClone}
+										style={{ color: "#4a82db" }}>
+										<ModalHeader toggle={this.toggleModalMeetupClone}>meetup.com Clone</ModalHeader>
+										<ModalBody>
+											<p> meetup.com Clone </p>
+											<p> React </p>
+											<p> Vanilla Js </p>
+											<p> Bootstrap </p>
+											<p> HTML & CSS </p>
+											<p> WordPress </p>
+											<p> PHP </p>
+										</ModalBody>
+										<ModalFooter>
+											<Link to="#">
+												<Button color="info">Live Demo</Button>
+											</Link>
+										</ModalFooter>
+									</Modal>
+								</Card>
+							</div>
 						</div>
 					</div>
-
-					<div className="col-xs-12 col-sm-12 col-md-6 col-lg-4">
-						<div>
-							<Card className="projectCard">
-								<CardImg
-									top
-									width="200"
-									height="200"
-									className="card-img"
-									src="https://amart31.github.io/andymartinez/meetup-clone.png"
-									alt="Card image cap"
-								/>
-								<CardBody>
-									<CardTitle>meetup.com Clone</CardTitle>
-									<CardSubtitle>React | Vanilla Js | WP | PHP</CardSubtitle>
-
-									<Button color="info" onClick={this.toggleModalMeetupClone}>
-										See More
-									</Button>
-								</CardBody>
-
-								<Modal
-									isOpen={this.state.modalMeetupClone}
-									toggle={this.toggleModalMeetupClone}
-									style={{ color: "#4a82db" }}>
-									<ModalHeader toggle={this.toggleModalMeetupClone}>meetup.com Clone</ModalHeader>
-									<ModalBody>
-										<p> meetup.com Clone </p>
-										<p> React </p>
-										<p> Vanilla Js </p>
-										<p> Bootstrap </p>
-										<p> WordPress </p>
-										<p> PHP </p>
-									</ModalBody>
-									<ModalFooter>
-										<Link to="#">
-											<Button color="info">Live Demo</Button>
-										</Link>
-									</ModalFooter>
-								</Modal>
-							</Card>
-						</div>
+					<div className="jumbotron text-center" style={{ marginBottom: "0" }}>
+						<p>Footer</p>
 					</div>
-				</div>
-				<div className="row m-2">
-					<div className="col-xs-12 col-sm-12 col-md-6 col-lg-4">
-						<div>
-							<Card className="projectCard">
-								<CardImg
-									top
-									width="200"
-									height="200"
-									className="card-img"
-									src="https://amart31.github.io/andymartinez/excuse-generator.png"
-									alt="Card image cap"
-								/>
-								<CardBody>
-									<CardTitle>Excuse Generator site</CardTitle>
-									<CardSubtitle>Vanilla Js | HTML | CSS | Bootstrap</CardSubtitle>
-
-									<Button color="info" onClick={this.toggleModalReactCloset}>
-										See More
-									</Button>
-								</CardBody>
-
-								<Modal
-									isOpen={this.state.modalReactCloset}
-									toggle={this.toggleModalReactCloset}
-									style={{ color: "#4a82db" }}>
-									<ModalHeader toggle={this.toggleModalReactCloset}>
-										Excuse Generator Vanilla Js
-									</ModalHeader>
-									<ModalBody>
-										<p> Excuse Generator site </p>
-										<p> React </p>
-										<p> Vanilla Js </p>
-										<p> Bootstrap </p>
-										<p> HTML & CSS </p>
-									</ModalBody>
-									<ModalFooter>
-										<a href="https://amart31.github.io/excuse-generator/">
-											<Button color="info">Live Demo</Button>
-										</a>
-									</ModalFooter>
-								</Modal>
-							</Card>
-						</div>
-					</div>
-
-					<div className="col-xs-12 col-sm-12 col-md-6 col-lg-4">
-						<div>
-							<Card className="projectCard">
-								<CardImg
-									top
-									width="200"
-									height="200"
-									className="card-img"
-									src="https://amart31.github.io/andymartinez/instagram-bstrap.png"
-									alt="Card image cap"
-								/>
-								<CardBody>
-									<CardTitle>Instagram Photofeed</CardTitle>
-									<CardSubtitle>HTML, CSS, & Bootstrap</CardSubtitle>
-
-									<Button color="info" onClick={this.toggleModalToDoApp}>
-										See More
-									</Button>
-								</CardBody>
-
-								<Modal
-									isOpen={this.state.modalToDoApp}
-									toggle={this.toggleModalToDoApp}
-									style={{ color: "#4a82db" }}>
-									<ModalHeader toggle={this.toggleModalToDoApp}>React Closet App</ModalHeader>
-									<ModalBody>
-										<p> React Modal </p>
-										<p> React </p>
-										<p> Vanilla Js </p>
-										<p> Bootstrap </p>
-										<p> HTML & CSS </p>
-										<p> WordPress </p>
-										<p> PHP </p>
-									</ModalBody>
-									<ModalFooter>
-										<a href="https://amart31.github.io/Instagram-photo-grid-clone/">
-											<Button color="info">Live Demo</Button>
-										</a>
-									</ModalFooter>
-								</Modal>
-							</Card>
-						</div>
-					</div>
-
-					<div className="col-xs-12 col-sm-12 col-md-6 col-lg-4">
-						<div>
-							<Card className="projectCard">
-								<CardImg
-									top
-									width="200"
-									height="200"
-									className="card-img"
-									src="https://amart31.github.io/andymartinez/meetup-clone.png"
-									alt="Card image cap"
-								/>
-								<CardBody>
-									<CardTitle>meetup.com Clone</CardTitle>
-									<CardSubtitle>React, Vanilla Js, WP, PHP</CardSubtitle>
-
-									<Button color="info" onClick={this.toggleModalMeetupClone}>
-										See More
-									</Button>
-								</CardBody>
-
-								<Modal
-									isOpen={this.state.modalMeetupClone}
-									toggle={this.toggleModalMeetupClone}
-									style={{ color: "#4a82db" }}>
-									<ModalHeader toggle={this.toggleModalMeetupClone}>meetup.com Clone</ModalHeader>
-									<ModalBody>
-										<p> meetup.com Clone </p>
-										<p> React </p>
-										<p> Vanilla Js </p>
-										<p> Bootstrap </p>
-										<p> HTML & CSS </p>
-										<p> WordPress </p>
-										<p> PHP </p>
-									</ModalBody>
-									<ModalFooter>
-										<Link to="#">
-											<Button color="info">Live Demo</Button>
-										</Link>
-									</ModalFooter>
-								</Modal>
-							</Card>
-						</div>
-					</div>
-				</div>
-				<div className="jumbotron text-center" style={{ marginBottom: "0" }}>
-					<p>Footer</p>
 				</div>
 			</React.Fragment>
 		);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -14,36 +14,38 @@ export class Home extends React.Component {
 		return (
 			<React.Fragment>
 				<HeroView />
-				<div className="row about-sec">
-					<div className="col text-center">
-						<About />
+				<div className="container-fluid">
+					<div className="row about-sec">
+						<div className="col text-center">
+							<About />
+						</div>
 					</div>
-				</div>
-				<div className="row projects-sec">
-					<div className="col text-center">
-						<h2 className="text-secondary">Projects</h2>
-						<Projects />
+					<div className="row projects-sec">
+						<div className="col text-center">
+							<h2 className="text-secondary">Projects</h2>
+							<Projects />
+						</div>
 					</div>
-				</div>
-				<div className="row contact-sec">
-					<div className="col text-center">
-						<h2 className="text-secondary">Contact</h2>
+					<div className="row contact-sec">
+						<div className="col text-center">
+							<h2 className="text-secondary">Contact</h2>
 
-						<Contact />
+							<Contact />
+						</div>
 					</div>
-				</div>
 
-				<div className="jumbotron text-center jumbo-footer">
-					<div className="d-inline-flex social-icon">
-						<i className="fab fa-linkedin fa-3x text-info" />
-					</div>
-					&nbsp;&nbsp;
-					<div className="d-inline-flex social-icon">
-						<i className="fab fa-github-square fa-3x text-info" />
-					</div>
-					&nbsp;&nbsp;
-					<div className="d-inline-flex social-icon">
-						<i className="fab fa-twitter-square fa-3x text-info" />
+					<div className="jumbotron text-center jumbo-footer">
+						<div className="d-inline-flex social-icon">
+							<i className="fab fa-linkedin fa-3x text-info" />
+						</div>
+						&nbsp;&nbsp;
+						<div className="d-inline-flex social-icon">
+							<i className="fab fa-github-square fa-3x text-info" />
+						</div>
+						&nbsp;&nbsp;
+						<div className="d-inline-flex social-icon">
+							<i className="fab fa-twitter-square fa-3x text-info" />
+						</div>
 					</div>
 				</div>
 			</React.Fragment>
