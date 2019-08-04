9622115ef3234a89cc801a70f8fe80c084854c79
Andy Martinez <martinez1751@gmail.com>
2019-08-04 11:58:20 -0400

    clean-up

--- src/js/component/imgCollage.js
+++ src/js/component/imgCollage.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 import reactCloset from "../../img/react-closet.jpg";
-import reactReseort from "../../img/react-resort.jpg";
+import excuseGenerator from "../../img/excuse-generator.png";
 import igClone from "../../img/instagram-bstrap.jpg";
+import TodoApp from "../../img/todo-app.png";
 
 import { DiHtml5, DiReact, DiJsBadge, DiWordpress, DiSass, DiBootstrap, DiCss3, DiNodejsSmall } from "react-icons/di";
 
@@ -45,36 +46,60 @@ class ImgCollage extends React.Component {
 					<div className="split">
 						<div className="col-6">
 							<div className="composition">
-								<img
-									src={igClone}
-									onMouseEnter={this.handleMouseHover}
-									onMouseLeave={this.handleMouseHover}
-									alt="instagram-clone"
-									className="composition__photo composition__photo--p1"
-								/>
-
-								<img
-									src={reactCloset}
-									onMouseEnter={this.handleMouseHover}
-									onMouseLeave={this.handleMouseHover}
-									alt="react-closet"
-									className="composition__photo composition__photo--p2"
-								/>
-
-								<img
-									src={reactReseort}
-									onMouseEnter={this.handleMouseHover}
-									onMouseLeave={this.handleMouseHover}
-									alt="react-resort"
-									className="composition__photo composition__photo--p3"
-								/>
+								<a
+									target="_blank"
+									rel="noopener noreferrer"
+									href="https://amart31.github.io/Instagram-photo-grid-clone/">
+									<img
+										src={igClone}
+										onMouseEnter={this.handleMouseHover}
+										onMouseLeave={this.handleMouseHover}
+										alt="instagram-clone"
+										className="composition__photo composition__photo--p1"
+									/>
+								</a>
+								<a
+									target="_blank"
+									rel="noopener noreferrer"
+									href="https://github.com/amart31/final-project-frontend">
+									<img
+										src={reactCloset}
+										onMouseEnter={this.handleMouseHover}
+										onMouseLeave={this.handleMouseHover}
+										alt="react-closet"
+										className="composition__photo composition__photo--p2"
+									/>
+								</a>
+								<a
+									target="_blank"
+									rel="noopener noreferrer"
+									href="https://amart31.github.io/excuse-generator/">
+									<img
+										src={excuseGenerator}
+										onMouseEnter={this.handleMouseHover}
+										onMouseLeave={this.handleMouseHover}
+										alt="react-resort"
+										className="composition__photo composition__photo--p3"
+									/>
+								</a>
+								<a
+									target="_blank"
+									rel="noopener noreferrer"
+									href="https://amart31.github.io/todo-app-react-hooks/">
+									<img
+										src={TodoApp}
+										onMouseEnter={this.handleMouseHover}
+										onMouseLeave={this.handleMouseHover}
+										alt="react-resort"
+										className="composition__photo composition__photo--p4"
+									/>
+								</a>
 							</div>
 						</div>
 						<div className="col-6 m-auto">
 							{this.state.isHovering && (
 								<div className="info-card">
 									<h5>Interactive and Scalable Solutions</h5>
-
 									<p>
 										<DiHtml5 /> <DiReact />
 										<DiJsBadge /> <DiWordpress /> <DiSass /> <DiBootstrap /> <DiCss3 />{" "}
@@ -93,15 +118,13 @@ class ImgCollage extends React.Component {
 									alt="instagram-clone"
 									className="composition__photo composition__photo--p1"
 								/>
-
 								<img
 									src={reactCloset}
 									alt="react-closet"
 									className="composition__photo composition__photo--p2"
 								/>
-
 								<img
-									src={reactReseort}
+									src={excuseGenerator}
 									alt="react-resort"
 									className="composition__photo composition__photo--p3"
 								/>
